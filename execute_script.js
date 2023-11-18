639fdc1a4be4906e0001de8436d23bad5e01ca7c
/script <65987444+FinnBaltazar1111@users.noreply.github.com>
2023-11-18 12:15:11 -0800

    Update execute_script.js

--- execute_script.js
+++ execute_script.js
@@ -1,6 +1,7 @@
-/// execute_script.js
-window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 192) {
-    eval(prompt("js to execute:"));
-  }
-})
+window.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("keyup", event => {
+    if (event.ctrlKey && event.which === 192) {
+      eval(prompt("js to execute:"));
+    }
+  })
+    }, false);
